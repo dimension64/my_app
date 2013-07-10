1c5ee82026bee8b6a136f542a1ebde4d1740d91f
dimension64 <64dimensions@gmail.com>
2013-07-10 11:59:09 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyApp::Application.config.secret_key_base = 'e6082f6c671fa4323fd45335231452c1a0361234e20fd105407402de9c7705a82c842ccd6cadb932a96d0315d1ad73e10b7252538d02cd85eff2924e4183b4f8'
